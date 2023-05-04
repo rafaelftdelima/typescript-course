2f8e73183e1daf6eb7bd66e8cd7279a02c3f623e
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-05-04 09:36:55 -0300

    0201 Annotation e Inference 1

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 "use strict";
-const welcome = 'Welcome, Luisiana';
-console.log(welcome);
+let product = 'Book';
+let price = 14.97;
+const car = {
+    color: 'black',
+    price: 64549.97,
+};
+const isCheap = car.price < 75000 ? true : '???';
+function sum(a, b) {
+    return a + b;
+}
+sum(4, 7);
+const nintendo = {
+    name: 'Nintendo',
+    price: '2249.47',
+};
+function turnPrice(price) {
+    return Number(price);
+}
+console.log(turnPrice(nintendo.price));
--- index.ts
+++ index.ts
@@ -1,2 +1,29 @@
-const welcome = 'Welcome, Luisiana';
-console.log(welcome);
+let product = 'Book';
+let price: number = 14.97;
+
+const car: {
+  color: string,
+  price: number,
+} = {
+  color: 'black',
+  price: 64549.97,
+};
+
+const isCheap: boolean | string = car.price < 75000 ? true : '???';
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(4, 7);
+
+const nintendo = {
+  name: 'Nintendo',
+  price: '2249.47',
+};
+
+function turnPrice(price: string): number {
+  return Number(price);
+}
+
+console.log(turnPrice(nintendo.price));
